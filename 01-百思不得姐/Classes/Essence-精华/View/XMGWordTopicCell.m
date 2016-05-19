75b764f6948365b0f45701e8ff6b9ffb7fd71c58
R了个G <3451059659@qq.com>
2016-05-19 20:08:59 +0800

    cell的基本数据

--- 01-百思不得姐/Classes/Essence-精华/View/XMGWordTopicCell.m
+++ 01-百思不得姐/Classes/Essence-精华/View/XMGWordTopicCell.m
@@ -9,6 +9,63 @@
 #import "XMGWordTopicCell.h"
 #import "XMGWordTopic.h"
 #import <UIImageView+WebCache.h>
+@interface XMGWordTopicCell ()
+/**
+ *  头像
+ */
+@property (weak, nonatomic) IBOutlet UIImageView *profileImageView;
+/**
+ *  姓名
+ */
+@property (weak, nonatomic) IBOutlet UILabel *nameLabel;
+/**
+ *  创建时间
+ */
+@property (weak, nonatomic) IBOutlet UILabel *createTimeLabel;
+/**
+ *  顶
+ */
+@property (weak, nonatomic) IBOutlet UIButton *dingButton;
+/**
+ *  踩
+ */
+@property (weak, nonatomic) IBOutlet UIButton *caiButton;
+/**
+ *  分享/转发
+ */
+@property (weak, nonatomic) IBOutlet UIButton *shareButton;
+/**
+ *  评论
+ */
+@property (weak, nonatomic) IBOutlet UIButton *commentButton;
+
+/**
+ *添加好友
+ */
+- (IBAction)clickFollow:(UIButton *)sender;
+
+/**
+ *顶一下
+ */
+- (IBAction)clickDing:(UIButton *)sender;
+
+/**
+ *踩一下
+ */
+- (IBAction)clickCai:(UIButton *)sender;
+
+/**
+ *分享
+ */
+- (IBAction)clickShare:(UIButton *)sender;
+
+/**
+ *评论
+ */
+- (IBAction)clickComment:(UIButton *)sender;
+
+@end
+
 @implementation XMGWordTopicCell
 - (void)awakeFromNib
 {
@@ -24,15 +81,15 @@
     //姓名
     _nameLabel.text = _wordTopic.name;
     //时间
-    _createTimeLabel.text = _wordTopic.created_at;
+    [self setLabelWithTime:_wordTopic.created_at];
     //顶
-    [_dingButton setTitle:[NSString stringWithFormat:@"%zd",_wordTopic.ding] forState:UIControlStateNormal];
+    [self setTitleWithButton:_dingButton count:_wordTopic.ding placeTitle:@"顶"];
     //踩
-    [_caiButton setTitle:[NSString stringWithFormat:@"%zd",_wordTopic.cai] forState:UIControlStateNormal];
+    [self setTitleWithButton:_caiButton count:_wordTopic.cai placeTitle:@"踩"];
     //分享
-    [_shareButton setTitle:[NSString stringWithFormat:@"%zd",_wordTopic.repost] forState:UIControlStateNormal];
+    [self setTitleWithButton:_shareButton count:_wordTopic.repost placeTitle:@"分享"];
     //评论
-    [_commentButton setTitle:[NSString stringWithFormat:@"%zd",_wordTopic.comment] forState:UIControlStateNormal];
+    [self setTitleWithButton:_commentButton count:_wordTopic.comment placeTitle:@"评论"];
 }
 
 - (IBAction)clickFollow:(UIButton *)sender {
@@ -57,5 +114,25 @@
     frame.size.width -= 2 * margin;
     frame.size.height -= margin;
     [super setFrame:frame];
+}
+#pragma mark - 对按钮文字做处理的方法
+- (void)setTitleWithButton:(UIButton *)button count:(NSInteger)count placeTitle:(NSString *)placeTitle
+{
+    if (count>=10000) {
+        placeTitle = [NSString stringWithFormat:@"%.1f万",count / 10000.0];
+    }else if (count>0) {
+        placeTitle = [NSString stringWithFormat:@"%zd",count];
+    }
+    [button setTitle:placeTitle forState:UIControlStateNormal];
+}
+#pragma mark - 对时间做处理
+- (void)setLabelWithTime:(NSString *)time
+{
+    //获取当前系统时间
+    NSDate *date = [NSDate date];
+    NSDateFormatter *formatter = [[NSDateFormatter alloc]init];
+    formatter.dateFormat = @"yyyy--MM--dd HH:mm:ss";
+//    NSString *dateString = [formatter stringFromDate:date];
+//    XMGLog(@"%@------%@",time,dateString);
 }
 @end
