91260050e0b8997dfc1ee8574f89e9d8f37c313a
R了个G <3451059659@qq.com>
2016-05-26 08:30:27 +0800

    pop实现弹出动画

--- 01-百思不得姐/Classes/Other-其他/Controller/XMGPublishViewController.m
+++ 01-百思不得姐/Classes/Other-其他/Controller/XMGPublishViewController.m
@@ -11,6 +11,8 @@
 #import <POP.h>
 //列数
 static NSInteger const cols = 3;
+static CGFloat const XMGAnimationDelay = 0.1;
+static CGFloat const XMGSpringFactor = 10;
 @interface XMGPublishViewController ()
 - (IBAction)cancel;
 @end
@@ -18,14 +20,11 @@ static NSInteger const cols = 3;
 @implementation XMGPublishViewController
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //动画完成前,禁止用户操作
+    self.view.userInteractionEnabled = NO;
     NSArray *images = @[@"publish-video",@"publish-picture",@"publish-text",@"publish-audio",@"publish-review",@"publish-offline"];
     NSArray *titles = @[@"发视频",@"发图片",@"发段子",@"发声音",@"审贴",@"离线下载"];
-    //添加标语
-    UIImageView *slogan = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"app_slogan"]];
-    slogan.y = XMGScreenHeight * 0.2;
-    slogan.centerX = XMGScreenWidth * 0.5;
-    [self.view addSubview:slogan];
-    
+
     //计算间距
     CGFloat buttonW = 70;
     CGFloat buttonH = buttonW + 30;
@@ -36,26 +35,49 @@ static NSInteger const cols = 3;
     //按钮间距
     CGFloat margin = (XMGScreenWidth - 2*startX - cols * buttonW) / (cols-1);
     CGFloat buttonX = 0;
-    CGFloat buttonY = 0;
+    CGFloat buttonEndY = 0;
     //添加按钮
     for (NSInteger i = 0; i<images.count; i++) {
+        XMGVerticalButton *button = [[XMGVerticalButton alloc]init];
+        [self.view addSubview:button];
+        //设置标题和图片
+        [button setTitle:titles[i] forState:UIControlStateNormal];
+        button.titleLabel.font = [UIFont systemFontOfSize:14];
+        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+        [button setImage:[UIImage imageNamed:images[i]] forState:UIControlStateNormal];
+        
         //行数
         NSInteger row = i / cols;
         //列数
         NSInteger col = i % cols;
         //设置位置和大小
         buttonX = col*(buttonW+margin)+startX;
-        buttonY = startY+row * buttonH;
-        XMGVerticalButton *button = [[XMGVerticalButton alloc]init];
-        button.frame = CGRectMake(buttonX, buttonY, buttonW, buttonH);
-        
-        //设置标题和图片
-        [button setTitle:titles[i] forState:UIControlStateNormal];
-        button.titleLabel.font = [UIFont systemFontOfSize:14];
-        [button setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
-        [button setImage:[UIImage imageNamed:images[i]] forState:UIControlStateNormal];
-        [self.view addSubview:button];
+        buttonEndY = startY+row * buttonH;
+        POPSpringAnimation *spring = [POPSpringAnimation animationWithPropertyNamed:kPOPViewFrame];
+        spring.fromValue = [NSValue valueWithCGRect:CGRectMake(buttonX, startY-XMGScreenHeight, buttonW, buttonH)];
+        spring.toValue = [NSValue valueWithCGRect:CGRectMake(buttonX, buttonEndY, buttonW, buttonH)];
+        spring.springSpeed = XMGSpringFactor;
+        spring.springBounciness = XMGSpringFactor;
+        spring.beginTime = CACurrentMediaTime()+XMGAnimationDelay * i;
+        [button pop_addAnimation:spring forKey:nil];
     }
+    
+    //添加口号
+    UIImageView *slogan = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"app_slogan"]];
+    [self.view addSubview:slogan];
+    slogan.center = CGPointMake(XMGScreenWidth * 0.5, -XMGScreenHeight);
+    
+    //计算位置
+    POPSpringAnimation *spring = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
+    spring.fromValue = [NSValue valueWithCGPoint:CGPointMake(XMGScreenWidth * 0.5,  -XMGScreenHeight)];
+    spring.toValue = [NSValue valueWithCGPoint:CGPointMake(XMGScreenWidth * 0.5, XMGScreenHeight * 0.2)];
+    spring.springBounciness = XMGSpringFactor;
+    spring.springSpeed = XMGSpringFactor;
+    spring.beginTime = CACurrentMediaTime() + images.count * XMGAnimationDelay;
+    [spring setCompletionBlock:^(POPAnimation *animation, BOOL finished) {
+        self.view.userInteractionEnabled = YES;
+    }];
+    [slogan pop_addAnimation:spring forKey:nil];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -67,20 +89,5 @@ static NSInteger const cols = 3;
 - (IBAction)cancel {
     [self dismissViewControllerAnimated:YES completion:nil];
     
-}
-- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
-{
-    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
-    btn.frame = CGRectMake(100, 100, 100, 100);
-    [self.view addSubview:btn];
-    btn.backgroundColor = [UIColor redColor];
-    POPSpringAnimation *spring = [POPSpringAnimation animationWithPropertyNamed:kPOPViewCenter];
-    spring.fromValue = [NSValue valueWithCGPoint:btn.center];
-    spring.toValue = [NSValue valueWithCGPoint:CGPointMake(300, 400)];
-//    spring.velocity = @10;
-    spring.springBounciness = 20;
-    spring.springSpeed = 20;
-    
-    [btn pop_addAnimation:spring forKey:@"spring"];
 }
 @end
